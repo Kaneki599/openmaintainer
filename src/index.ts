8f97ff267db0ad796a3c39592b57a99847bded26
Kaneki <78763389+Kaneki599@users.noreply.github.com>
2026-08-08 22:56:38 +0000

    config: support justified rule suppressions

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./types.js";
 export { scanRepository } from "./scanner.js";
 export { checkWorkflowSecurity } from "./rules/workflow-security.js";
 export { formatMarkdown } from "./reporters.js";
+export { loadConfig } from "./config.js";
